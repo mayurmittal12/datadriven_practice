8a4b80bd4314d4a77365deaf2aae4a207d663bb4
mayurmittal12 <mayur.mittal@gradeup.co>
2019-11-11 16:58:51 +0530

    second commit

--- src/test/java/com/w2a/listeners/CustomListener.java
+++ src/test/java/com/w2a/listeners/CustomListener.java
@@ -1,33 +1,33 @@
-package com.w2a.listeners;
-
-import com.relevantcodes.extentreports.LogStatus;
-import com.w2a.base.TestBase;
-//import com.w2a.utilities.TestUtil;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-import org.testng.Reporter;
-
-import java.io.IOException;
-
-public class CustomListener extends TestBase implements ITestListener {
-    public void onTestStart(ITestResult iTestResult) {
-
+//package com.w2a.listeners;
+//
+//import com.relevantcodes.extentreports.LogStatus;
+//import com.w2a.base.TestBase;
+////import com.w2a.utilities.TestUtil;
+//import org.testng.ITestContext;
+//import org.testng.ITestListener;
+//import org.testng.ITestResult;
+//import org.testng.Reporter;
+//
+//import java.io.IOException;
+//
+//public class CustomListener extends TestBase implements ITestListener {
+//    public void onTestStart(ITestResult iTestResult) {
+//
 //        System.out.println("ewsfcqews : "+iTestResult.getTestName().toUpperCase());
 //        test = repo.startTest(iTestResult.getTestName().toUpperCase());
- //       System.out.println(iTestResult.getName());
-    }
-
-    public void onTestSuccess(ITestResult iTestResult) {
-
+//        System.out.println(iTestResult.getName());
+//    }
+//
+//    public void onTestSuccess(ITestResult iTestResult) {
+//
 //        test.log(LogStatus.PASS, iTestResult.getName().toUpperCase()+"PASS");
 //        repo.endTest(test);
 //        repo.flush();
-
-    }
-
-    public void onTestFailure(ITestResult iTestResult) {
-
+//
+//    }
+//
+//    public void onTestFailure(ITestResult iTestResult) {
+//
 //        System.setProperty("org.uncommons.reportng.escape-output","false");
 //
 //        try {
@@ -36,35 +36,35 @@ public class CustomListener extends TestBase implements ITestListener {
 //            e.printStackTrace();
 //        }
 //
-////        test.log(LogStatus.FAIL, iTestResult.getName().toUpperCase()+"Failed with exception:"+iTestResult.getThrowable());
-////
-////        test.log(LogStatus.FAIL, test.addScreenCapture(TestUtil.screenshotname));
+//        test.log(LogStatus.FAIL, iTestResult.getName().toUpperCase()+"Failed with exception:"+iTestResult.getThrowable());
+//
+//        test.log(LogStatus.FAIL, test.addScreenCapture(TestUtil.screenshotname));
 //
 //        Reporter.log("Capturing the image");
 //        Reporter.log("<a target=\"_blank\" href="+TestUtil.screenshotname+"> Screenshot </a>");
 //        Reporter.log("<br>");
 //        Reporter.log("<a target=\"_blank\" href="+TestUtil.screenshotname+"><img src="+TestUtil.screenshotname+" height=400 width=400></img></a>");
 //
-////           repo.endTest(test);
-////           repo.flush();
-
-
-    }
-
-    public void onTestSkipped(ITestResult iTestResult) {
-
-    }
-
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
-    }
-
-    public void onStart(ITestContext iTestContext) {
-
-
-    }
-
-    public void onFinish(ITestContext iTestContext) {
-
-    }
-}
+//           repo.endTest(test);
+//           repo.flush();
+//
+//
+//    }
+//
+//    public void onTestSkipped(ITestResult iTestResult) {
+//
+//    }
+//
+//    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+//
+//    }
+//
+//    public void onStart(ITestContext iTestContext) {
+//
+//
+//    }
+//
+//    public void onFinish(ITestContext iTestContext) {
+//
+//    }
+//}
